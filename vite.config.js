9c0d8c63cf44af3349401b26e0c0bf7fe5f0fa31
Antun Karlovac <antun@antunkarlovac.com>
2023-06-10 10:53:53 -0700

    Use noExternal to avoid errors with react-live-runner and react-runner packages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
 
 export default {
-  plugins: [react(), ssr({ prerender: true })]
+  plugins: [react(), ssr({ prerender: true })],
+  ssr: {
+    noExternal: ['react-live-runner', 'react-runner']
+  }
 }
