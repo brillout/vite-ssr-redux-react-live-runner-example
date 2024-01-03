5c7e9cd12a7fd300927d0d064cf6b86cbbb4b912
Romuald Brillout <git@brillout.com>
2024-01-03 16:16:54 +0100

    reproduce original error

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import ssr from 'vite-plugin-ssr/plugin'
 export default {
   plugins: [react(), ssr({ prerender: true })],
   ssr: {
-    noExternal: ['react-live-runner', 'react-runner']
+    // noExternal: ['react-live-runner', 'react-runner']
   }
 }
